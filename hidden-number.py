5561d6c0fd44794af4fd6f7c1ac6e37163447d58
DomenOslaj <doslaj@gmail.com>
2019-02-19 00:17:08 +0100

    while True + brake added

--- hidden-number.py
+++ hidden-number.py
@@ -1,11 +1,13 @@
 secret = 7
 
-guess = raw_input("Which number do i have in mind? :) ")
+while True:
+    guess = raw_input("Which number do i have in mind? :) ")
 
-guess = int(guess)
+    guess = int(guess)
 
-if guess == secret:
-    print ("Congratulations, that is the right number!")
+    if guess == secret:
+        print ("Congratulations, that is the right number!")
+        break
 
-else:
-    print ("Nope, try again! :)")
+    else:
+        print ("Nope, try again! :)")
